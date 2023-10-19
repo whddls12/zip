af799f6dc58782876499707f8c88052f642909aa
최제혁 <cjh970324@naver.com>
2023-10-19 15:14:15 +0900

    init: entity 초안 생성
    Former-commit-id: 892448a0321e043b69cbde60189df865f09560fc

--- backend/ziip/src/main/java/com/lastdance/ziip/member/repository/entity/Member.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/member/repository/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0382c4699f3daefe1826253cdd285a5be22ed03dcd21e1cc5dc8d9c101550afb
-size 1987
+oid sha256:4b4d688c2b743bbf5b5c3a8373b6acf988cec653967429e699a6bc1f717c1ba3
+size 2014
--- backend/ziip/src/main/java/com/lastdance/ziip/schedule/repository/entity/SchedulePhoto.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/schedule/repository/entity/SchedulePhoto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db6fdb93f89975cba43d1a14fb35694d8adb9cce30082cf391bb2020e5585a15
-size 983
+oid sha256:bb8c57b5f323014be5be1affbd25efb376a36043048128d627add51ee93f3f63
+size 1053
