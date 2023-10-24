96ed5219879b0bf6ea268021aba16d8dda91a564
최제혁 <cjh970324@naver.com>
2023-10-24 17:11:04 +0900

    feat: MemberController 어노테이션
    Former-commit-id: 8a3149b9d35423763b0116cda391002ace558b99

--- backend/ziip/src/main/java/com/lastdance/ziip/member/controller/MemberController.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/member/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.lastdance.ziip.member.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/members")
 public class MemberController {
 
 }
