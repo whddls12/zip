da6ba2a3a304d37dca68b951a56e5e3d34192924
이형석 <gudtjr2949@naver.com>
2023-10-25 15:52:48 +0900

    feat: 메인화면 일정 리스트 조회
    Former-commit-id: a3949e5c1c2ed86972fd1afbb37f67b04c5b0603

--- frontend/zip-app/screens/FamilyMainScreen.js
+++ frontend/zip-app/screens/FamilyMainScreen.js
@@ -1,27 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function FamilyMainScreen() {
-	return (
-		<View style={styles.container}>
-			<View style={styles.family}>
-				<Text>가족 메인 화면</Text>
-			</View>
-		</View>
-	);
-}
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	family: {
-		borderWidth: 1,
-		borderColor: 'black',
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: 300,
-		height: 400,
-	},
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:306f0f92fca0cbe6e1bf9f7b0ce7524c103cffa8757cc612fd3347f235a8308b
+size 2079
--- frontend/zip-app/screens/navigation/StackNavigator.js
+++ frontend/zip-app/screens/navigation/StackNavigator.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5147f1b48fe5b0b0bde09c84d574c52c957c8107f479c1860641729059fb22d1
-size 1195
+oid sha256:8d0394f7d620d79d6bf3ea59ff9940cdc194e0756feede3c0b6cc92afa7dbf5a
+size 1204
