2ab6447eff2e02d9ed94b41debde546c7c12599a
이형석 <gudtjr2949@naver.com>
2023-10-26 17:33:52 +0900

    feat: 가족 관련 수정버튼 추가
    Former-commit-id: 1fe5e3787c780f536053ba3a0f439ae24b8805f4

--- frontend/zip-app/screens/FamilyMainScreen.js
+++ frontend/zip-app/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5def0e1021f522cbde0314ed8abdf50ce762e450ee555d05280497939fd1b306
-size 3851
+oid sha256:ebfa156153a255b8b59e6a9464e261b0c84d32cb0fccec09fef9d93fa4337807
+size 5852
