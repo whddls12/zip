2a6979ee40480a640ead8eecd59f317574148cbf
choijehyuk <cjh970324@naver.com>
2023-10-26 23:09:16 +0900

    feat: 가족 초대 수락 스웨거 어노테이션 구현완료 S09P31B206-121
    Former-commit-id: e05de172ec17882ff7f447c03ac2e891534395d9

--- backend/ziip/src/main/java/com/lastdance/ziip/family/controller/FamilyController.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/family/controller/FamilyController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e97305a29ef37041a1d7bb4eea61b37d12773c2929d42f16f48ea70ae31c189
-size 4328
+oid sha256:f0f1ceb8065c584d73e9b7ae482aae564fe102d1f45b3f8e0c842428b9dad310
+size 4443
