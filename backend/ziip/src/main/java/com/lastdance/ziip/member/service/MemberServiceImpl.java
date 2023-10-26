d526ea740ee38592872120106285501107bb703e
최제혁 <cjh970324@naver.com>
2023-10-26 10:47:30 +0900

    fix: 회원정보 업데이트 임시수정
    Former-commit-id: c06af50caf6d5c7c207341eb310872bf8f05fb0b

--- backend/ziip/src/main/java/com/lastdance/ziip/member/service/MemberServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03836d38bec004814e29355ace4125ad0fbdd57cd26a6bc46fe765c8d278795f
-size 16098
+oid sha256:0ce866481f8e68199fb721c28a04f6e5d02d22ab7129ddb74ad792c842f5e675
+size 16323
