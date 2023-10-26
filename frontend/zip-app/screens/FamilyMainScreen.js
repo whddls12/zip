f3d09d1650a0fc876e56b5ae7c7aed8bcb224698
이형석 <gudtjr2949@naver.com>
2023-10-26 16:28:13 +0900

    feat: 메인화면 가족 이름, 소개,  배경 사진, 프로필 사진 추가
    Former-commit-id: 1489c8ba8b089c47f85db712528324cfe47cb746

--- frontend/zip-app/screens/FamilyMainScreen.js
+++ frontend/zip-app/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5fecad67a440ad0d96f08ecf34eda1381fcd6e5f5bdcafcb357b521b7853a28
-size 3350
+oid sha256:5def0e1021f522cbde0314ed8abdf50ce762e450ee555d05280497939fd1b306
+size 3851
