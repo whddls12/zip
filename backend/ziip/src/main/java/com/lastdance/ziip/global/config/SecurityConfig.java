adc6b860a1fe21bfbb0b762bc5348994cf355e67
choijehyuk <cjh970324@naver.com>
2023-10-28 17:58:47 +0900

    env: security 설정 변경
    Former-commit-id: 83e4eb3349e473894f33445456f0ee2346483833

--- backend/ziip/src/main/java/com/lastdance/ziip/global/config/SecurityConfig.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b9e9025ad1b5a95e19ba0dd1d5cb204a34e7c93e420d6cf81aa42bbdf559ca8
-size 2693
+oid sha256:ffc8f7ff0daf0e040abfadf152c0fa6be1798e56c0777a9cd336514576750d86
+size 2712
