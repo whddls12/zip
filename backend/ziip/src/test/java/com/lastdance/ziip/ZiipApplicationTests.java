e98bcca7beccd878e4951c032a3097be2ef2d302
hajin618 <1998rlagkwls@gmail.com>
2023-10-28 17:23:26 +0900

    error: test 주석
    Former-commit-id: 2ee5c6ffeb990a67edfcfc5fb23b3866d416a1f5

--- backend/ziip/src/test/java/com/lastdance/ziip/ZiipApplicationTests.java
+++ backend/ziip/src/test/java/com/lastdance/ziip/ZiipApplicationTests.java
@@ -1,13 +1,13 @@
-package com.lastdance.ziip;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ZiipApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.lastdance.ziip;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ZiipApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
