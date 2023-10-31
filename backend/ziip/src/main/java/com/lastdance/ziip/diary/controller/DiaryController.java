1a9db32fdd5dd1803b46db8e0edeb08eb4d8fd0d
이 은 석 <modelism93@naver.com>
2023-10-31 13:18:59 +0900

    refactor : 필요없는 로깅 제거
    Former-commit-id: 60ffd4f02396e04c63f51d864ec0a7805f3a3b3c

--- backend/ziip/src/main/java/com/lastdance/ziip/diary/controller/DiaryController.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/controller/DiaryController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27c8218d4b046ec9098536ea3cf9a7d498f9ff83dd8b11cf6ce2938ad67a9a71
-size 2616
+oid sha256:70d17f4c641f809b64ec8df35020190a3a4a8b8bd6e986b584dda157b8f57d98
+size 2434
