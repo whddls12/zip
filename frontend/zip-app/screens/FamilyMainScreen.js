417861d6d17b14d469299e06ec4eebbaa713ae75
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2023-11-02 23:49:49 +0900

    feat: 이미지 수정용 모달창 CSS 구현중
    Former-commit-id: 5b6024e3028539e66c879f5dd5e3c9cee11e829b

--- frontend/zip-app/screens/FamilyMainScreen.js
+++ frontend/zip-app/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b04665649a79b2367cf5712d67501594d938819e248dca01f32ed77fd09abf6e
-size 14394
+oid sha256:4b39b2df08d638b16d0f7385d79c5d35c7d497d52ae76c5ecbeb0c80369b99b5
+size 14829
