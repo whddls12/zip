c86768b37290d400d5be4fea49f24c7bcdafe37d
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2023-11-02 23:12:45 +0900

    feat: 사진 변경용 모달창 출력
    Former-commit-id: d2ab132f65703ee6ea9b46f7aaba9c9803202156

--- frontend/zip-app/screens/FamilyMainScreen.js
+++ frontend/zip-app/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a02715cff6ba2bde2eb9064392190348051f99918783e42cb6affbe9c5d67e95
-size 11480
+oid sha256:b04665649a79b2367cf5712d67501594d938819e248dca01f32ed77fd09abf6e
+size 14394
