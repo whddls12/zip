f916a9f0c8b0512e00f81c9e9b6725e02ad59e42
hajin618 <1998rlagkwls@gmail.com>
2023-11-02 10:19:50 +0900

    feat: plan content 관련 api 삭제
    Former-commit-id: 1d7208a8173d47fc69616d60ae64f026b1768384

--- backend/ziip/src/main/java/com/lastdance/ziip/schedule/service/ScheduleServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/schedule/service/ScheduleServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:421f5e8424bb47e7c5ba4eedfa2486455c648ce945ed54dae39f133d86d8c9e0
-size 6108
+oid sha256:e84f09b1eaeb53b23620bfb50b084ea023ca2dd9d6540e2e872df7fc9490c1e7
+size 6060
