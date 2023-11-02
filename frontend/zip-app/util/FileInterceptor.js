69253d07c3007fad920d2af44b596b6ca4afcc60
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2023-11-02 20:20:47 +0900

    feat:
    Former-commit-id: 3140b55f675ab6588cfa20f0a7f9735fc4feb901

--- frontend/zip-app/util/FileInterceptor.js
+++ frontend/zip-app/util/FileInterceptor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60c8f97253710c7c5b3b98cf1ad367742251ba2509b90291cc9ba9dab799ab4b
+oid sha256:ca96b8a5b62f0e306a8f35048f7b657b6e64d64abc51809dee64b89b371687d2
 size 1631
