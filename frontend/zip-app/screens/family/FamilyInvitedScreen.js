9e6d6d7f65a86833ba141600e82dd8aaeaee49ef
이형석 <gudtjr2949@naver.com>
2023-11-03 17:36:32 +0900

    feat: 가족 초대 입력 화면 추가
    Former-commit-id: 67b06fde504b1d936c0d921b1e7535074c7cbf19

---
+++ frontend/zip-app/screens/family/FamilyInvitedScreen.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b80cb449fa56a088e7f562865b658634de050f25ecb936e336e62fa8cc6c17d0
+size 3274
--- frontend/zip-app/screens/navigation/StackNavigator.js
+++ frontend/zip-app/screens/navigation/StackNavigator.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17ffb10059fb2bd7eae1cb4b314605f9a09d271643e7e0c6b6fe8b8445e21e77
-size 1411
+oid sha256:501897cd663824ff99eafa283dd4a9fa90656ac81e603f70fd732709ee2a64be
+size 1603
