30f778e270937ddd8d09c55893844ee1d5a32139
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2023-11-06 02:15:16 +0900

    feat: 가족 초대 메시지 전송 List 수정
    Former-commit-id: 48cd800156184635f5a11d4dee1ae01c4ed90d24

--- backend/ziip/src/main/java/com/lastdance/ziip/family/service/FamilyServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/family/service/FamilyServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4c74f171d6e92fb6709331e9db9da72288812ed422c129b5d14964e1e5ccd87
-size 11606
+oid sha256:c7b4ced842ae764bc5705fac60948228358611b34bbf3d93f1edbe4e0c12cfa4
+size 11676
