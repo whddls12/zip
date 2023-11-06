15c9a1174848126c07fb8614e021bdc8da9cfff7
이 은 석 <modelism93@naver.com>
2023-11-06 17:13:03 +0900

    fix : 일기 등록시 Request에 사진 없어도 저장되도록 코드 수정
    Former-commit-id: e838eaa5d05e8ae4c7aa20ed29a3f889016def0b

--- backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d90a99019e88975e510ba9b08398a4f95ece37d9ecb8eefc99aa7695d89c1e7
-size 7042
+oid sha256:609026afb51212ebd5cc51d02e1e34499b554e139d8a79c75e52a3ab0f5269b0
+size 7402
