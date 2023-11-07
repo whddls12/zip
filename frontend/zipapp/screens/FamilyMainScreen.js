b6728f7f343cc2bdf940fa86eb6b12586149edb5
이형석 <gudtjr2949@naver.com>
2023-11-07 23:08:47 +0900

    feat: 갤러리 접근 수정, 모달창 범위 밖 터치 시, 모달 비활성화 기능 추가
    Former-commit-id: 02e1d81e5167546414dc59c71a1628dd304b94b2

--- frontend/zipapp/screens/FamilyMainScreen.js
+++ frontend/zipapp/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e28cc62a85a2320175dff7d96a1e1473614680d9ecfc0d7f027176ac322b2c5
-size 18127
+oid sha256:17da402077aaa402a841bbb673d13a38482fb049cc0f9194cb56c1b43328a029
+size 18008
