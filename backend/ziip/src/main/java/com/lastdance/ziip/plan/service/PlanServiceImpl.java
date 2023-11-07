9cd4c6d4e65c3221f14f7246c8f5f53e12a817f2
hajin618 <1998rlagkwls@gmail.com>
2023-11-07 13:05:48 +0900

    feat: 계획 생성시 담당자 변경
    Former-commit-id: fecb3f6c4396f61de5f0ea3460f215840379060e

--- backend/ziip/src/main/java/com/lastdance/ziip/plan/service/PlanServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/plan/service/PlanServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c899faa0148afca54f42238a6dae8ab670cf1286e791a597536f511a760f04e
-size 5458
+oid sha256:9bef5cdfc97863b448a8ca48e9e46137b7f58614d49da174392bb3bc78ecc7ef
+size 5572
