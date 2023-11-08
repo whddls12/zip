a85af30d1ca56eb30c7668c921a81cc615276f67
이 은 석 <modelism93@naver.com>
2023-11-08 15:49:51 +0900

    fix : 일정 삭제 requestParam으로 변경
    Former-commit-id: 41ed01391afa48963e9059d81d519a6a436b9365

--- backend/ziip/src/main/java/com/lastdance/ziip/plan/controller/PlanController.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/plan/controller/PlanController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:713f242ca84079834a934d7d351a87fea55ee9f9453903e98c83ceedd1b7cd8e
-size 6343
+oid sha256:eeee8d0e593960026a5c01d4b3db8909024cfed792501ac94051249f2b4304c2
+size 6300
