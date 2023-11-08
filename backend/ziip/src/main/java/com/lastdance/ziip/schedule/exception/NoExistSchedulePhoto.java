f056ebe6b40f3998508447b98a9102458d8c870d
이 은 석 <modelism93@naver.com>
2023-11-08 17:29:47 +0900

    feat : schedulePhoto에 Validator 설정
    Former-commit-id: 023502d43f0c034e34b3e7c36f0a4b37c8cacd8c

---
+++ backend/ziip/src/main/java/com/lastdance/ziip/schedule/exception/NoExistSchedulePhoto.java
@@ -0,0 +1,5 @@
+package com.lastdance.ziip.schedule.exception;
+
+public class NoExistSchedulePhoto extends RuntimeException{
+    public NoExistSchedulePhoto(String message){super(message);}
+}
---
+++ backend/ziip/src/main/java/com/lastdance/ziip/schedule/exception/validator/ScheduleValidator.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:28de751e42e33a57a4fa3bc0d28015ec08a7a69e4d9f03286a91c82a9acc6126
+size 918
