d4babd97fd16a70926ed0319081a5e5f055d7f57
이 은 석 <modelism93@naver.com>
2023-11-08 17:28:06 +0900

    feat : schedulePhoto에 Member 엔티티 연결
    Former-commit-id: f74747f41ad7a283d89b896ba6e8e1f07be07daa

--- backend/ziip/src/main/java/com/lastdance/ziip/member/repository/entity/Member.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/member/repository/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:235e71a80fdfbe323c2f8533ced2d274b372f328d07738cd3718329c0f6db887
-size 3008
+oid sha256:5514951dd8478aba2e4bd069ccf62c66282f99283df3fd5c30ef54d51f1fa5ef
+size 3215
