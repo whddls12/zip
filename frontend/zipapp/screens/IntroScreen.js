946977642125ad63159ffcf2da7470ddfca7e0b2
이형석 <gudtjr2949@naver.com>
2023-11-08 13:04:32 +0900

    Conflict 해결
    Former-commit-id: 4a6ae36a6be8694457335feb1341eb69c3d5e7f4

--- frontend/zipapp/screens/IntroScreen.js
+++ frontend/zipapp/screens/IntroScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43613a5d90e50668d5602794edd94eaf3ff3c0ed58a38977a1a229a8c266c834
-size 3594
+oid sha256:bc3a1e3a709ffba4a4c90f59bb21cefd4deaf267f5b15f22d8dd05c0c4f9c2d6
+size 3406
