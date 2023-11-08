e0d6798df9d30e412a1c49fd8327da92b4952087
이형석 <gudtjr2949@naver.com>
2023-11-09 00:44:20 +0900

    feat: FCM Token 수신 테스트
    Former-commit-id: a56eafd7f7500afbe86b9010c329beecccbfb4d5

--- backend/ziip/src/main/java/com/lastdance/ziip/family/service/FamilyServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/family/service/FamilyServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cb18f9b7b1796ea4cac9b7c0fd45afb327cc2907b2a51e941d483128f40f7e9
-size 12244
+oid sha256:863f8233a37b643496d51a82a5c631ce23e226a6501e83bd36c93cd6ca94b13e
+size 12296
---
+++ backend/ziip/src/main/java/com/lastdance/ziip/notification/controller/NotificationController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4429f0757d7f3ec3bf00f91c2f16c36023ab23a152a3af8035cbcdbbddbad1c2
+size 1907
