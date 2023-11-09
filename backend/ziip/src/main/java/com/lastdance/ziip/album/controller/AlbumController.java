db471640e77cf6a952a4690f52f54340783d9c92
이 은 석 <modelism93@naver.com>
2023-11-09 14:27:52 +0900

    feat: 사진 전체 조회 api , 테스트용 로그 삭제
    Former-commit-id: 75c13b2e4d9b47c552f58be213407c3778819ccd

--- backend/ziip/src/main/java/com/lastdance/ziip/album/controller/AlbumController.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/album/controller/AlbumController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b1f3a6850c478c51f1e26aeda3875d2d0f79db13eecc08885ae03d4eaaea611
-size 2290
+oid sha256:5cdf941a023c24d19a6f2e353a3de3ba20d20f028cd0cd5cfe2ce5939e7ee9ca
+size 2253
