eec33bbb3cfc7e3326cc38fff48f60b94d66431e
최제혁 <cjh970324@naver.com>
2023-11-13 17:20:10 +0900

    fix: 환경변수 주석
    Former-commit-id: 2e1d70453d3206d27990cc983b850cb4c5791019

--- backend/ziip/src/main/java/com/lastdance/ziip/global/auth/oauth2/Messaging.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/global/auth/oauth2/Messaging.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6319f7040ebce2efd633168be4bbd844a5e44fe00e090598eab2709ee2b5ff33
-size 1521
+oid sha256:2369d8970344e355c2c32b3c5f22e44ca2062c6c5ee8650096ce80745a33470c
+size 1527
