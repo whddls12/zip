4febdef34bd2a2ec52f80f59875f75f0db89beb4
이 은 석 <modelism93@naver.com>
2023-11-13 22:16:34 +0900

    fix : diary 삭제 body에서 param으로 변경
    Former-commit-id: fdabac41b094250c0080e08403c90c934b99bea2

--- backend/ziip/src/main/java/com/lastdance/ziip/diary/controller/DiaryController.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/controller/DiaryController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b91be40a11c488831c4b6240a9e51bb003e0817fea6c780d5ba0efff4aa61827
-size 7494
+oid sha256:cc549662ca8b6d8fd5e9bf068cd32129fd0d491ceb99bbc5fd7e790d7cd39e76
+size 7450
--- backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71df1ba1a44ed012ecbd4ddd9d435ad0bb840f4a715e84e123f41f88a33c0657
-size 10575
+oid sha256:ab1deff6faea8e5a91953a4e16892854b267e5a6efe744d4bed08b13eb25f646
+size 10517
