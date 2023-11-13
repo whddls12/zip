d24a471a9b8685cb9c8fcdd188471de76a648f5d
최제혁 <cjh970324@naver.com>
2023-11-13 09:44:25 +0900

    fix: 카카오 restApi key 변경 S09P31B206-286
    Former-commit-id: 57348d2f80c61282f5bee8cd732cbc83a8e896cc

--- backend/ziip/src/main/java/com/lastdance/ziip/global/auth/oauth2/kakao/KakaoOAuth2.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/global/auth/oauth2/kakao/KakaoOAuth2.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2f93f0f7f0835aec49ec8c65cbb66079489e8de4ab3cd046ec4912165b40b13
-size 4007
+oid sha256:05e02732c4cf7f09c7113535d1717e0a3df9e7beb0157f77326ae09966eeefd3
+size 3949
