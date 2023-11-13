923c3a1a731926a7ff5a121aa55fbe985ef656f3
이형석 <gudtjr2949@naver.com>
2023-11-14 00:03:09 +0900

    feat: 일정 리스트 유저 닉네임, 메인화면에서 가족 리스트로 이동하는 버튼 추가
    Former-commit-id: d030abaaca977f7ece513383c05fefd68b805308

--- frontend/zipapp/screens/FamilyMainScreen.js
+++ frontend/zipapp/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e6ffec2b4286b1b189759ea172cc33daba2a0563a56a308539f183042b3121f
+oid sha256:58958b7ffbc6efc40b32348a5f80f23cd58bf7b3ee3d4e914305c23608366df1
 size 18985
--- frontend/zipapp/screens/family/FamilyInsertScreen.js
+++ frontend/zipapp/screens/family/FamilyInsertScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d17ce4f9ae74bff3444ed59bf190a44d853c5cd2cb43d6859f50d308d135a2a
-size 9338
+oid sha256:45182fa15f58c0e96c8e32f9fe2be4fe98ebfebebd4ccd124410a722806abb94
+size 10357
