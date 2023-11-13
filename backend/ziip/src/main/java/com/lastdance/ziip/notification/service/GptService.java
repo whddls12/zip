590a75996d108363837306413483b09990462c9d
최제혁 <cjh970324@naver.com>
2023-11-13 13:46:47 +0900

    env: 타임존 변경
    Former-commit-id: a36828c29a9140050c51739baeac4da45216e56c

--- backend/ziip/src/main/java/com/lastdance/ziip/notification/service/GptService.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/notification/service/GptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9a4af869cbf8b5269126dbe8887601e6dca62140278ad9df54ff6f6a30d0231
+oid sha256:93afa056924bfb5928c8f2f1db3d5441b48e181f518746700a27da2113b2b817
 size 2977
