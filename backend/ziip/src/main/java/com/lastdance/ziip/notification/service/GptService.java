8160453af9f1b37e07e9d538ef6a1b9f32de3b08
choijehyuk <cjh970324@naver.com>
2023-11-13 15:00:16 +0900

    fix: 스케줄러
    Former-commit-id: 1003f171c5ff592ec924085dce53106af614c50f

--- backend/ziip/src/main/java/com/lastdance/ziip/notification/service/GptService.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/notification/service/GptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dac08781e286094cab0d272821a6a1d03d66ac290380e246f01dd0cbac21a77
-size 2903
+oid sha256:a90d0dbd1c58eb8c8c3d6622a7abe1c0bd2c708d7b2c17d798ca445b1567842f
+size 2901
