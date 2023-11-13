3cb2aa43a96f82749f88c57c59f66b16ba27f2f5
이 은 석 <modelism93@naver.com>
2023-11-13 17:25:38 +0900

    feat : diary comment 작성자 Id response에 추가
    Former-commit-id: 4160c8ad3edb35f1725b15d0f573081bb6e3aaea

--- backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c24090cb7ee18d3df869343f101b6e72ae4a30fdbb425eb30e95d7234f507e44
-size 10496
+oid sha256:71df1ba1a44ed012ecbd4ddd9d435ad0bb840f4a715e84e123f41f88a33c0657
+size 10575
