d693bd4f8de82c6e69934fe5596ae30947a6d1a9
hajin618 <1998rlagkwls@gmail.com>
2023-11-13 13:54:09 +0900

    fix: 1분마다
    Former-commit-id: 5dd150c46926ba831b6f029f0b908fa379234de6

--- backend/ziip/src/main/java/com/lastdance/ziip/notification/service/GptService.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/notification/service/GptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93afa056924bfb5928c8f2f1db3d5441b48e181f518746700a27da2113b2b817
+oid sha256:45f0ce04c69930a1d7bf3cc518abdb2a8a379646d3466f2fef1230b0ab9d1fb7
 size 2977
