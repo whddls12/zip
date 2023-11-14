9e8f6f8be63e7c930046174a30fff53ac7ec4c2e
이형석 <gudtjr2949@naver.com>
2023-11-14 17:36:21 +0900

    feat: 텍스트 정합 검사 코드 추가
    Former-commit-id: 4dd81189b3e1bc72bee148f19a47f7c0028b4aa7

--- frontend/zipapp/screens/family/FamilyInsertScreen.js
+++ frontend/zipapp/screens/family/FamilyInsertScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45182fa15f58c0e96c8e32f9fe2be4fe98ebfebebd4ccd124410a722806abb94
-size 10357
+oid sha256:01f2db4ebd790f144aea9e2f91945b10cef1ed3f0a88460834f8e8dc7be84a5b
+size 10523
