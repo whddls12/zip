792554f2d230ad2b6f32df738d78b89114bd189b
이형석 <gudtjr2949@naver.com>
2023-11-14 15:06:36 +0900

    feat: 일기 리스트 유저 닉네임, 프로필사진 리턴 S09P31B206-261
    Former-commit-id: 51f7e9824db2cf91a38c1c4acf6dbf48f85bd653

--- backend/ziip/src/main/java/com/lastdance/ziip/diary/dto/response/DiaryListDetailResponseDto.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/dto/response/DiaryListDetailResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad82cbf8dcaa8eab29d2624c58ebb6d1de123b15f180c4a512a413139fac292f
-size 671
+oid sha256:adc2e2eba27529e92459d8e4249cc06bc562d0831cf81e5ee486d4be636530ba
+size 705
--- backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab1deff6faea8e5a91953a4e16892854b267e5a6efe744d4bed08b13eb25f646
-size 10517
+oid sha256:c33d1ae66e39c57704fdd7e9663f7f9dfc547fd6437f6a749c84fbc08041b0b0
+size 9585
