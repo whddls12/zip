815b953aa35466833ce5f9fc6d2e936e0df96726
이형석 <gudtjr2949@naver.com>
2023-11-15 00:57:33 +0900

    feat: 일정, 일기 리스트 조회 시 갯수 제한, 마이페이지 수정, 배경화면 수정 버튼 위치 변경
    Former-commit-id: 535e47089ecc15247072a8a4c25e53937f490f75

--- frontend/zipapp/components/check/LengthText.js
+++ frontend/zipapp/components/check/LengthText.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb01a0e74b5d6911459992bff6a58390068d7664fd2b4d6d92f2ffcad355217d
-size 1000
+oid sha256:d4118c6e1e56871a9775ffbeaf17ed2015c48ddd64ca71bb00e29f1926d76be7
+size 781
