f5686cfb6ca38bf7e137bb57e3baa52006ead7b4
이형석 <gudtjr2949@naver.com>
2023-11-15 00:17:48 +0900

    feat: 로그아웃, 모달창 구현, CSS 수정
    Former-commit-id: c4ea770db1ebd5e7f6e33a34874341da12e4316a

--- frontend/zipapp/screens/FamilyMainScreen.js
+++ frontend/zipapp/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58958b7ffbc6efc40b32348a5f80f23cd58bf7b3ee3d4e914305c23608366df1
-size 18985
+oid sha256:ea3d6445988517f34b0c740dc4a991351e22b8fa51f400069bdac9801d747878
+size 18984
