e46d8dfd2044c0d84c6fc63941ad3ba88fc371a3
이 은 석 <modelism93@naver.com>
2023-11-15 12:49:41 +0900

    feat: web/dev first commit
    Former-commit-id: 168547fee575afbf4995db7bbcf34f59d128b824

---
+++ web/zipweb/src/components/download/DownloadButton.tsx
@@ -0,0 +1,10 @@
+const DownloadButton = () => {
+
+  return(
+    <>
+      i'm 메인 버튼이에요
+    </>
+  )
+}
+
+export default DownloadButton
---
+++ web/zipweb/src/pages/download/DownloadPage.tsx
@@ -0,0 +1,12 @@
+import DownloadButton from "../../components/download/DownloadButton"
+
+const DownloadPage = () => {
+  return(
+    <>
+      <p>i'm메인 페이지에요</p>
+      <DownloadButton/>
+    </>
+  )
+}
+
+export default DownloadPage
