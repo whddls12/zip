258b016bcd0352947e5072f26c968a012fc55086
이형석 <gudtjr2949@naver.com>
2023-11-15 10:20:31 +0900

    feat: 기본 배경사진 변경, 초대 코드 입력 비활성화 기능 추가
    Former-commit-id: 12c7daa314c1245c02f462d293e0d363f31ccc9c

--- frontend/zipapp/util/Interceptor.js
+++ frontend/zipapp/util/Interceptor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2992ed1d19a3ab3219a1c3aabfd3a345ef16426c62b428765bd932cf138156f4
-size 1791
+oid sha256:7970f20b056c1e6de75c438c668b04f4bb5ef3ae13eac1a682be7b057d653e9a
+size 1873
