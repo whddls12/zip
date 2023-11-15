747dfd9a1afcd068643c6381aad1c9dbf34f676e
이형석 <gudtjr2949@naver.com>
2023-11-16 02:18:57 +0900

    feat: sdk 버전 확인
    Former-commit-id: ca76de1d0045a69fdf996e04310bed83f532d805

--- frontend/zipapp/screens/FamilyMainScreen.js
+++ frontend/zipapp/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68091cfb4ca89bbc67db640713aa5475cf6c314b178cc55abcbc0fcdfe8dc329
-size 21321
+oid sha256:03388187141f520a7a48c052d54b4f93647923d04a8324fd997d712f130b6964
+size 21529
