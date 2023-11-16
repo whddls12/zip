5feeaaedc75c9867d381b8017064c89d57e3bbe0
choijehyuk <cjh970324@naver.com>
2023-11-16 22:56:44 +0900

    remove: gpt폴더 삭제
    Former-commit-id: 41250ad85e5cde45859e0a83ff5f63306d1712b8

--- gpt/gpt/schemas/gpt_request.py
+++
@@ -1,6 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-
-class GptRequest(BaseModel):
-    question: Optional[str] = None
--- gpt/gpt/services/gpt_service.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f6ccba3200919f84ef57eaf3525db1c22762216c1aeaf475635cc310b53ff8fc
-size 643
