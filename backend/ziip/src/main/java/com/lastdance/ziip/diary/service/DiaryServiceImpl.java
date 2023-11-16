2f45e0f64531d048b1a564b34fb624482280047b
이 은 석 <modelism93@naver.com>
2023-11-16 20:39:26 +0900

    fix: diaryPhotos get(0) 문제 해결
    Former-commit-id: ea815299f7bdf921b5be5b6888d4993e22aa6f2b

--- backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c6b6bc3194f08df0934b344410cd90b3a549a9a8b86caf06c62b55654b0cb6b
-size 10456
+oid sha256:d03a46fda7700ddb14c5ccf2b52f8ac9a28dd723f5371d47c04548c4f58ef0e0
+size 10373
