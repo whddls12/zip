f2725e0a729446ddbd65e10b12063e8fdb8106be
이 은 석 <modelism93@naver.com>
2023-11-16 10:54:05 +0900

    feat: 다운로드 아이콘 추가
    Former-commit-id: d00cd4b07363a597bee2d682ba1d0e3bb2c8d5f0

--- web/zipweb/src/components/download/Image.tsx
+++ web/zipweb/src/components/download/Image.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6ca2349a891a6cec93d09ed89dce5382f7a8660132d48dd798a8c353767a6d4
-size 1702
+oid sha256:a65db9b6268af0609066476fdb63a263bcfc43abe5c2864be31657ec7d1d8ae6
+size 1917
--- web/zipweb/src/pages/download/DownloadPage.tsx
+++ web/zipweb/src/pages/download/DownloadPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:899a52e757736ce7b8d55d3a6734b8d70e8bbc38addf9aa4303599e6633cd1ee
-size 1181
+oid sha256:f749fe91c04adf321285b7b9e0e331768cc455af96cee6056a15268e9fdcebf7
+size 1154
