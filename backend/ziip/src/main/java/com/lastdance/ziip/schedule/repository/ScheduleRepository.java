7b072c53928f31abcb236f11f8fb0b44ec95d58d
최제혁 <cjh970324@naver.com>
2023-11-16 17:35:12 +0900

    refactor: 미사용 코드 제거
    Former-commit-id: 0c577520729a3195d183a410efd2e98695b4b6ad

--- backend/ziip/src/main/java/com/lastdance/ziip/schedule/repository/ScheduleRepository.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/schedule/repository/ScheduleRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1daed4e45f0bb0423046fc3d9ac9db4c00afb6f06a6eede29b79717cd1039016
-size 1298
+oid sha256:289a77743324c47e05dddc2748d66f0cda7fb2180bdc50d719738be963b430d3
+size 922
