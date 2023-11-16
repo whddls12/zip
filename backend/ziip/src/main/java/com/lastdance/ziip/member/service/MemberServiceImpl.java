38b12a35ed7a0a0edc3482a2fff09fac022ff76c
이형석 <gudtjr2949@naver.com>
2023-11-16 15:47:24 +0900

    feat: 회원정보 수정 시, FCM 토큰 저장
    Former-commit-id: c15a663b7ac9ff2357e5579bfa7ef342071219fe

--- backend/ziip/src/main/java/com/lastdance/ziip/member/service/MemberServiceImpl.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ecf39469370dd2404a9d8c69877faa011ece115e18e21804d8795dea37d3bf5
-size 16734
+oid sha256:8af2fbc3005b2fd014eb62cb41598cdf1c891f266a3cae67fec4bc997b0a35bb
+size 16838
