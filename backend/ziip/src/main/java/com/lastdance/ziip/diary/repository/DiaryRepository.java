66272bfa0507ebc207330023741b1580387224d4
이형석 <gudtjr2949@naver.com>
2023-11-16 19:52:39 +0900

    feat: 내 일기 리스트 출력 수정
    Former-commit-id: 5e029f0ca5757422e02d57c0f97a35f0552f3279

--- backend/ziip/src/main/java/com/lastdance/ziip/diary/repository/DiaryRepository.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/diary/repository/DiaryRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ed0c8622108afa492916bdcf1369d4fd40af4f0354066bef1aa042527e7870e
-size 860
+oid sha256:dca2b53541d5dc3ce72abb95e0489de7167938d96ead2d9b16d650f5b28e1dcd
+size 1048
