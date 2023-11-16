b0819b5b462bb8aa56e0b9123a379cea7d94c3f6
이형석 <gudtjr2949@naver.com>
2023-11-16 16:07:17 +0900

    feat: 이미지 수정
    Former-commit-id: c9f71198a9b8b22ad6fc91f56527084fc1cf09e9

--- frontend/zipapp/screens/FamilyMainScreen.js
+++ frontend/zipapp/screens/FamilyMainScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b604bd8b75c7234943dfa30f41070bc771cfb7ace96332d3394aab7cbcf12bf4
-size 21511
+oid sha256:f1f89aa7563a943f1b13109420c767d8a031555ccf5a5b819380487ce3bb0d8f
+size 21250
--- frontend/zipapp/screens/family/FamilySelectScreen.js
+++ frontend/zipapp/screens/family/FamilySelectScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8ff38f27ba1c04005fbe0d46ca1f7d9f9964cdfd7cde74510191f9759386fa8
-size 5764
+oid sha256:7712c27a421a5b463fcc87bad9e020817e0eb56c42dd7a7757922b8697a6426e
+size 5780
